f64d3bd8f76d207d87c44f8ea879a064582d7e4d
Henry Våg <henry.vag@turkuamk.fi>
2023-10-24 23:05:27 +0300

    GUI with class, stop app functionality

--- GUI.py
+++ GUI.py
@@ -1,44 +1,56 @@
 import tkinter as tk
 
-"""Defines window"""
-window = tk.Tk()
+class MyGUI:
 
-window.geometry("500x500")
-window.title("Listomator")
+    def __init__(self):
+        
+        """Defines window"""
+        self.window = tk.Tk()
+        self.window.geometry("500x500")
+        self.window.title("Listomator")
 
-label = tk.Label(window, text="Listomator", font=('Arial', 18))
-label.pack()
+        """Defines label"""
+        self.label = tk.Label(self.window, text = "Listomator")
+        self.label.pack(padx = 10, pady = 10)
+        """
+        self.button = tk.Button(self.window, text = "test")
+        self.button.pack()
+        """
 
-"""Buttons in window"""
+        """Functional buttonframe and buttons"""
 
-buttonframe = tk.Frame(window)
-buttonframe.columnconfigure(0, weight=1)
-buttonframe.columnconfigure(1, weight=1)
-buttonframe.columnconfigure(2, weight=1)
-
-button_1 = tk.Button(buttonframe, text="1")
-button_1.grid(row=0, column=0, sticky = "news")
-
-button_2 = tk.Button(buttonframe, text="2")
-button_2.grid(row=0, column=1, sticky = "news")
-
-button_3 = tk.Button(buttonframe, text="3")
-button_3.grid(row=0, column=2, sticky = "news")
-
-button_4 = tk.Button(buttonframe, text="4")
-button_4.grid(row=1, column=0, sticky = "news")
-
-button_5 = tk.Button(buttonframe, text="5")
-button_5.grid(row=1, column=1, sticky = "news")
-
-button_6 = tk.Button(buttonframe, text="6")
-button_6.grid(row=1, column=2, sticky = "news")
+        buttonframe = tk.Frame(self.window)
+        buttonframe.columnconfigure(0, weight=1)
+        buttonframe.columnconfigure(1, weight=1)
+        buttonframe.columnconfigure(2, weight=1)
 
 
+        button_1 = tk.Button(buttonframe, text="Stop Application", command = self.stop_app)
+        button_1.grid(row=0, column=0, sticky="news")
 
-buttonframe.pack(fill = 'x')
+        button_2 = tk.Button(buttonframe, text="Search")
+        button_2.grid(row=0, column=1, sticky="news")
+
+        button_3 = tk.Button(buttonframe, text="Add")
+        button_3.grid(row=0, column=2, sticky="news")
+
+        button_4 = tk.Button(buttonframe, text="Remove")
+        button_4.grid(row=1, column=0, sticky="news")
+
+        button_5 = tk.Button(buttonframe, text="Sort")
+        button_5.grid(row=1, column=1, sticky="news")
+
+        button_6 = tk.Button(buttonframe, text="List")
+        button_6.grid(row=1, column=2, sticky="news")
 
 
+        buttonframe.pack(fill='x')
 
-window.mainloop()
+        self.window.mainloop()
+
+    def stop_app(self):
+        
+        self.window.destroy()
+
+MyGUI()
 
