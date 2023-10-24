6ca157816426cd29775fc57dd304809b347f4894
Henry Våg <henry.vag@turkuamk.fi>
2023-10-24 20:10:22 +0300

    First Commit of assignment_4b

---
+++ assignment_4b.py
@@ -0,0 +1,28 @@
+fruits_and_vegetables = [
+    "Apple", "Banana", "Orange", "Strawberry", "Blueberry",
+    "Watermelon", "Pineapple", "Mango", "Grapes", "Kiwi",
+    "Peach", "Pear", "Lemon", "Lime", "Avocado",
+    "Tomato", "Cucumber", "Carrot", "Spinach", "Broccoli",
+    "Cauliflower", "Bell Pepper", "Eggplant", "Zucchini", "Sweet Potato",
+    "Onion", "Garlic", "Cabbage", "Lettuce", "Radish"
+]
+
+def sort_list():
+    fruits_and_vegetables.sort()
+    print(fruits_and_vegetables)
+
+
+
+def search_list():
+    user_input = input("Search:")
+
+    for items in fruits_and_vegetables:
+        if user_input == items:
+            print("found item")
+    if user_input != items:
+        print("item not found")    
+
+search_list()
+
+
+
