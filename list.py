7eb5ce69f8ca92043d860fc6497a09efa328a4e2
Henry Våg <henry.vag@turkuamk.fi>
2023-10-31 10:30:15 +0200

    import list added

--- list.py
+++ list.py
@@ -8,22 +8,10 @@ fruits_and_vegetables = ["Apple", "Banana", "Orange", "Strawberry", "Blueberry",
     "Cauliflower", "Bell Pepper", "Eggplant", "Zucchini", "Sweet Potato",
     "Onion", "Garlic", "Cabbage", "Lettuce", "Radish"]
 
-def sort_list():
-    fruits_and_vegetables.sort()
-    print(fruits_and_vegetables)
 
 
-"""
-def search_list():
-    user_input = input("Search:")
 
-    for items in fruits_and_vegetables:
-        if user_input == items:
-            print("found item")
-            
-    if user_input != items:
-        print("item not found")
-""" 
+
 
 
 
